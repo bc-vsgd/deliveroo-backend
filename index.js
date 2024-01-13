ce4ab2d61df1abf1795a3bc0aa0da17e3ed77935
Charles Benoît <charlesbenoit@MacBook-Pro-de-Charles.local>
2024-01-13 14:48:06 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "ok" });
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
